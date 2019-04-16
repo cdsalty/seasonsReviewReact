f1875b9e2096dfdbe27730c39985bba83c51d9c1
cdsalty <soltiscd@gmail.com>
2019-04-16 15:50:47 -0400

    setting first instance of state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position)=>console.log(position),
-        (err)=>console.log(err)
-    );
-    return <div>Hi There!</div>;
+// We originally started/continued with functional components and transitioned into Class/State
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position)=>console.log(position),
+//         (err)=>console.log(err)
+//     );
+//     return <div>Lattitude: </div>;
+// };
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {};
+    }
+
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err)=>console.log(err)
+        );
+        return <div>Lattitude: </div>
+    }
 };
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+
+// Added ASYNC/AWAIT to get position since the app component was displaying before I was getting the position.
